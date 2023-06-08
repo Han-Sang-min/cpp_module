47e78aad267e6c449e574520eac276e094328464
Sangmin Han <sangmiha@c2r5s8.42seoul.kr>
2023-06-08 17:54:56 +0900

    FEAT: ex00 & doing ex01..2

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -7,7 +7,6 @@ template <typename T>
 typename T::iterator easyfind(T& container, int value) {
     typename T::iterator it = std::find(container.begin(), container.end(), value);
 
-    // if not found end return check
     if (it == container.end()) {
         throw std::runtime_error("Value not found");
     }
--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -5,6 +5,8 @@
 class Span {
 	private:
 		std::vector<int> vec;
+		unsigned int N;
+
 	public:
 		Span();
 		Span(const Span &other);
@@ -16,4 +18,5 @@ class Span {
 		void addNumber(int value);
 		int shortestSpan() const;
 		int longestSpan() const;
+		void addRange(std::vector<int>::iterator st, std::vector<int>::iterator end);
 };
