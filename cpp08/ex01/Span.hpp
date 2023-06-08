7cbe654560c7d6304f14af93bc1992ac1e45c96d
한상민 <hansangmin@hansangmin-ui-MacBookAir.local>
2023-06-08 13:28:39 +0900

    FEAT: middle of ex02

--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -1,7 +1,19 @@
 #pragma once
 
+#include <vector>
+
 class Span {
 	private:
-
+		std::vector<int> vec;
 	public:
+		Span();
+		Span(const Span &other);
+		Span &operator=(const Span &other);
+		~Span();
+
+		Span(unsigned int n);
+
+		void addNumber(int value);
+		int shortestSpan() const;
+		int longestSpan() const;
 };
